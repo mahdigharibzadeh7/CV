1a6e0dc691bfb154f16b98915adca70631ebc38c
mahdigharibzadeh7 <gharibzadehmahdi@gmail.com>
2022-07-25 15:46:42 +0430

    full screen and refactor the code

--- Custom Video Player/script.js
+++ Custom Video Player/script.js
@@ -2,22 +2,12 @@ let media = document.querySelector("video");
 let play = document.querySelector(".play");
 let rwd = document.querySelector(".rewind");
 let fwd = document.querySelector(".forward");
-let crntTime = document.querySelector(".currenttime");
-let videoTime = document.querySelector(".videotime");
 let progressBar = document.querySelector(".progressbar-current");
-
-media.addEventListener("timeupdate", function () {
-  crntTime.textContent = setTime(media.currentTime);
-  let barLength = (media.currentTime / media.duration) * 100;
-  progressBar.style = `background: linear-gradient(90deg, rgba(230, 126, 34, 1) ${barLength}%, #e1e1e1 0%)`;
-  progressBar.value = barLength;
-});
-
-progressBar.addEventListener("input", function () {
-  media.currentTime = (this.value / 100) * media.duration;
-});
+let volume = document.querySelector("#volume_bar");
+let fullscreen = document.querySelector(".fullscreen");
 
 play.addEventListener("click", function () {
+  let videoTime = document.querySelector(".videotime");
   videoTime.textContent = setTime(media.duration);
   if (media.paused) {
     togglePlay({ show: true });
@@ -39,6 +29,35 @@ fwd.addEventListener("click", function () {
   media.currentTime += 5;
 });
 
+media.addEventListener("timeupdate", function () {
+  let crntTime = document.querySelector(".currenttime");
+  crntTime.textContent = setTime(media.currentTime);
+  let barLength = (media.currentTime / media.duration) * 100;
+  progressBar.value = barLength;
+});
+
+progressBar.addEventListener("input", function () {
+  media.currentTime = (this.value / 100) * media.duration;
+});
+
+volume.addEventListener("input", function () {
+  media.volume = this.value / 100;
+});
+
+fullscreen.addEventListener("click", function () {
+  let playerArea = document.querySelector(".myplayer");
+  let controls = document.querySelector(".controls-container");
+  if (!document.fullscreenElement) {
+    playerArea.requestFullscreen();
+    media.style.width = "100%";
+    controls.style.width = "100%";
+  } else {
+    document.exitFullscreen();
+    media.style.width = "900px";
+    controls.style.width = "900px";
+  }
+});
+
 function togglePlay({ show }) {
   let icon = play.querySelector("i");
   if (show) {
@@ -53,7 +72,9 @@ function togglePlay({ show }) {
 function setTime(time) {
   let minutes = Math.floor(time / 60);
   let seconds = Math.floor(time - minutes * 60);
+
   let minutesValue, secondsValue;
+
   if (minutes < 10) minutesValue = "0" + minutes;
   else minutesValue = minutes;
 
